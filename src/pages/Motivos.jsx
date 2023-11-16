7724e789fd57b5eac3c7ca51dd70cb5a687eee6b
sofia soares <Sofiateca37@gmail.com>
2023-11-16 08:41:44 -0300

    maisumcommit

--- src/pages/Motivos.jsx
+++ src/pages/Motivos.jsx
@@ -14,10 +14,10 @@ function Motivos() {
       <div className={styles.desktop}>
       <div className={styles.index}>
       <div className={styles.div}>
-        <div className={styles.overlap}>
+        {/* <div className={styles.overlap}> */}
           {/* <div className={styles.text_wrapper}>PyroBot</div> */}
-        </div>
-        <div className={styles.logod}>
+        {/* </div> */}
+        {/* <div className={styles.logod}> */}
         {/* <Link to={"/Inicio"}>
         <img
             className={styles.icone_pyro_bot}
@@ -25,49 +25,62 @@ function Motivos() {
             src={Logo}
           />
         </Link> */}
-        </div>
+        {/* </div> */}
         <div className={styles.overlap_2}>
-          <div className={styles.rectangle} />
-          <p className={styles.p}>Por que escolher o PyroBot?</p>
-          <img
-            className={styles.iconezinmotivos}
-            alt="Iconezinmotivos"
-            src={iconezinmotivos3}
-          />
-          <img
-            className={styles.img_2}
-            alt="Iconezinmotivos"
-            src={iconezinmotivos2}
-          />
+          {/* <div className={styles.rectangle} /> */}
+          
+          <h4 className={styles.p}>Por que escolher o PyroBot?</h4>
+          <div className={styles.textoeiconizin1}>
           <img
             className={styles.img}
             alt="Iconezinmotivos"
             src={iconezinmotivos1}
-          />
+          />    
           <p className={styles.confiabilidade}>
             Confiabilidade garantida
             <br />
             Performance confiável <br />
             Monitoramento contínuo
           </p>
+
+          <div className={styles.textoeiconizin2}>
+          <img
+            className={styles.img_2}
+            alt="Iconezinmotivos"
+            src={iconezinmotivos2}
+          />
           <p className={styles.resposta_r_pida}>
             Resposta rápida
             <br />
             Detecção precisa 24/7 <br />
             Alertas instantâneos
           </p>
+          </div>
+          
+          <div className={styles.textoeiconizin3}>
+          <img
+            className={styles.iconezinmotivos}
+            alt="Iconezinmotivos"
+            src={iconezinmotivos3}
+          />
           <p className={styles.detec_o_precisa}>
             Detecção precisa
             <br />
             Precisão incomparável <br />
             Eficiência exemplar
           </p>
-          <div className={styles.rectangle_2} />
+          </div>
+          </div>
+          
+          {/* <div className={styles.rectangle_2} /> */}
+          <div className={styles.pyroboteincrivel}>
           <div className={styles.text_wrapper_2}>PyroBot é simplesmente incrível!</div>
           <p className={styles.text_wrapper_3}>
             Sua tecnologia avançada de detecção de incêndios e resposta rápida garantem a máxima segurança.
           </p>
-          <img
+          </div>
+          
+          {/* <img
             className={styles.line}
             alt="Line"
             src="https://cdn.animaapp.com/projects/6521ab18d9cb42a4b3df616b/releases/6521ac5d62f75e9352093c3e/img/line-6.svg"
@@ -76,12 +89,12 @@ function Motivos() {
             className={styles.line_2}
             alt="Line"
             src="https://cdn.animaapp.com/projects/6521ab18d9cb42a4b3df616b/releases/6521ac5d62f75e9352093c3e/img/line-7.svg"
-          />
-          <img
+          /> */}
+          {/* <img
             className={styles.programadoraxinaisa}
             alt="Programadoraxinaisa"
             src="https://cdn.animaapp.com/projects/6521ab18d9cb42a4b3df616b/releases/6521ac5d62f75e9352093c3e/img/programadoraxinaisa-1.png"
-          />
+          /> */}
         </div>
       </div>
     </div>
--- tarefas.md
+++ tarefas.md
@@ -1,14 +1,14 @@
 [x] Colocar o menu no começo da página
 [] Estilizar o menu (trocar fonte, colocar hover)
 [] Deixar o cursor do mouse normal
-[] Trocar a imagem da Home para uma foto do projeto
+[x] Trocar a imagem da Home para uma foto do projeto
 [] Exterminar as rolagens horizontais de todas as páginas (cuidar com os tamanhos)
-[] Adicionar o nome dos integrantes abaixo das fotos
+[x] Adicionar o nome dos integrantes abaixo das fotos
 [x] Colocar o menu em TODAS as páginas
-[x] Colocar os preços das peças do projeto e o preço TOTAL do projeto
-[] Retirar todas as imagens que não tem relação com o projeto
+[] Colocar os preços das peças do projeto e o preço TOTAL do projeto
+[x] Retirar todas as imagens que não tem relação com o projeto
 [x] Arrumar o alinhamento da logo com o nome do projeto em todas as páginas
-[] Diminuir o tamanho das fontes do projeto (os avaliadores não são cegos)
+[x] Diminuir o tamanho das fontes do projeto (os avaliadores não são cegos)
 
 # Mobile
 [x] Colocar um menu hamburguer clicável que mostra os links das páginas
